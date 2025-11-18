706cee4d26ec90179a9fce592b3db7dc619bf1ca
Lian Shi <lians@student.chalmers.se>
2025-11-18 11:48:54 +0100

    #4 Add multiple pages and linking them

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 
 def login_view(request):
-    return render(request, '', {})
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("prediction:input")
+        else:
+            context ={"error": "Invalid username of password"}
+            return render(request, "accounts/login.html", context)
+
+    return render (request, "accounts/login.html")
+
 
 
 def logout_view(request):
-    return render(request, '', {})
+    return render(request, 'home')
 
 
 def register_view(request):
-    return render(request, '', {})
+    return render(request, 'accounts/register.html', {})
+
+def history_view(request):
+    return render(request, 'accounts/history.html')
--- apps/predictions/urls.py
+++ apps/predictions/urls.py
@@ -1,5 +1,10 @@
-# from django.urls import path
+from django.urls import path
+
+from apps.predictions import views
 
 app_name = 'predictions'
 
-urlpatterns = []
+urlpatterns = [
+    path('input', views.input_view, name ='input'),
+    path('result', views.result_view, name ='result')
+]
