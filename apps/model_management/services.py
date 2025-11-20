9f042b74c0d73cb87845ca001b0b92b570e90960
claudiasevilla <sevillaeslava@gmail.com>
2025-11-20 18:56:50 +0100

    #7 Modify DatasetRecord model and import_csv_dataset to support one-hot encoding of the new dataset categories
    Former-commit-id: 3ee5f621b1516db75d4c2896364238ecbce1a12b

--- apps/model_management/services.py
+++ apps/model_management/services.py
@@ -1,10 +1,18 @@
 import csv
-from django.db import transaction
 from apps.model_management.models import DatasetRecord, DataUpload
 
-def import_csv_dataset(file_path, data_upload, dataset_type='train', batch_size=1000):
+CATEGORY_MAP = {  # Define category mapping
+    "normal": 0,
+    "depression": 1,
+    "suicidal": 2,
+    "anxiety": 3,
+    "bipolar": 4,
+    "stress": 5,
+}
+
+def import_csv_dataset(file_path, data_upload, dataset_type='train', batch_size=5000):
     """
-    Imports a CSV file into DatasetRecord model.
+    Imports a CSV file into DatasetRecord model using one-hot-encoding and category_id.
     :param file_path: path to CSV file
     :param data_upload: DataUpload instance
     """
@@ -12,15 +20,30 @@ def import_csv_dataset(file_path, data_upload, dataset_type='train', batch_size=
 
     with open(file_path, newline='', encoding='utf-8') as f:
         reader = csv.DictReader(f)
+
         for row in reader:      # Map known columns to model fields
+            label = row.get('status', '').strip().lower()
+            category_id = CATEGORY_MAP.get(label, None)
+
+            one_hot_categories = {
+                "normal": 0,
+                "depression": 0,
+                "suicidal": 0,
+                "anxiety": 0,
+                "bipolar": 0,
+                "stress": 0,
+                } # Initialise all columns to 0
+
+            if category_id is not None:
+                one_hot_categories[label] = 1
+
             record = DatasetRecord(
-                text=row.get('text', ''),
-                label=int(row['label']) if row.get('label') else None,
-                subreddit=row.get('subreddit', ''),
-                confidence=float(row['confidence']) if row.get('confidence') else None,
-                features={k: v for k, v in row.items() if k not in ['text', 'label', 'subreddit', 'confidence']},
+                text=row.get('statement', ''),
+                label=row.get('status', ''),
+                category_id=category_id,
                 dataset_type=dataset_type, # (train, test, or unlabeled)
-                data_upload=data_upload
+                data_upload=data_upload,
+                **one_hot_categories #unpack into model fields
             )
             batch.append(record)
 
