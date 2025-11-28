222ba7d09810d382e7e923a6989c9a604b610df5
claudiasevilla <sevillaeslava@gmail.com>
2025-11-28 19:16:19 +0100

    #6 Modify data cleaning pipeline tests to have no DB dependencies
    Former-commit-id: cdf70d57549ba4f96335f08f0b7f51c2852a6419

--- apps/ml_admin/tests.py
+++ apps/ml_admin/tests.py
@@ -4,10 +4,9 @@ import tempfile
 import pandas as pd
 from django.test import TestCase
 
-from ml_pipeline.data_cleaning.cleaner import (
-    run_cleaning_pipeline,
-)
-from apps.ml_admin.models import DatasetRecord, DataUpload
+from ml_pipeline.data_cleaning.cleaner import DataCleaningPipeline
+
+# from ml_pipeline.preprocessing.preprocessor import DataPreprocessingPipeline
 
 
 class DataCleaningTests(TestCase):
@@ -35,7 +34,7 @@ class DataCleaningTests(TestCase):
             },  # Text is too long ( > 5000)
             {'text': '   ', 'label': 'Normal'},  # Whitespace only
             {'text': '', 'label': 'Normal'},  # Empty text
-            {'text': 'hello', 'label': 'Normal'}, # Text is too short (<10)
+            {'text': 'hello', 'label': 'Normal'},  # Text is too short (<10)
             # Invalid labels
             {'text': 'This is a valid text', 'label': ''},
             {'text': 'This is valid too', 'label': None},
@@ -47,23 +46,20 @@ class DataCleaningTests(TestCase):
             {'text': 'This text is twice', 'label': 'Depression'},
         ]
 
-        self.csv_file = 'text,label\n'  # Create CSV file
+    def create_csv_file(self):
+        csv_file = 'text,label\n'  # Create CSV file
         for row in self.test_data:
             text = row['text'] if row['text'] is not None else ''
             label = row['label'] if row['label'] is not None else ''
-            self.csv_file += f'"{text}","{label}"\n'
+            csv_file += f'"{text}","{label}"\n'
 
-    def create_test_upload(self):  # Method to create a DataUpload instance
         with tempfile.NamedTemporaryFile(mode='w', suffix='.csv', delete=False) as f:
-            f.write(self.csv_file)
-            temp_file_path = f.name
+            f.write(csv_file)
+            return f.name
 
-        upload = DataUpload.objects.create(file_path=temp_file_path)
-        return upload, temp_file_path
-
-    # Test complete dataflow of the pipeline (source file -> cleaning pipeline -> database)
+    # Test complete dataflow of the pipeline (source file -> cleaning pipeline)
     def test_data_flow(self):
-        upload, temp_file_path = self.create_test_upload()
+        temp_file_path = self.create_csv_file()
 
         try:
             # Verify source file exists and contains test data
@@ -73,20 +69,25 @@ class DataCleaningTests(TestCase):
             initial_row_count = len(
                 test_df
             )  # Keep track of the row count before processing
-            print(f"{initial_row_count} rows before cleaning")
+            print(f'{initial_row_count} rows before cleaning')
 
             # Run pipeline
-            outcome = run_cleaning_pipeline(upload.id)
-            self.assertTrue(outcome['success'], 'Error: pipeline execution failed')
-            self.assertIn('row_count', outcome, 'Error: missing row_count')  # Check rowcount exists
-            self.assertIn('report', outcome, 'Error: missing report')
-            # Check there is same or fewer rows after cleaning
-            self.assertLessEqual(outcome['row_count'], initial_row_count, 'Error: row count should decrease after cleaning')
+            cleaner = DataCleaningPipeline()
+            df_cleaned, report = cleaner.clean_file(temp_file_path)
+            self.assertTrue(len(df_cleaned) > 0, 'Error: pipeline execution failed')
 
-            # Check data was loaded to the database:
-            records = DatasetRecord.objects.filter(data_upload=upload)
-            self.assertEqual(len(records), outcome['row_count'], 'Error: mismatch in database record count')
-            print(f"{len(records)} records successfully saved in database")
+            self.assertIn(
+                'final_count', report, 'Error: missing final_count'
+            )  # Check final count exists
+            self.assertIn('report', report, 'Error: missing report')
+            # Check there is same or fewer rows after cleaning
+            self.assertLessEqual(
+                len(df_cleaned),
+                initial_row_count,
+                'Error: row count should decrease after cleaning',
+            )
+
+            print(f'{len(df_cleaned)} records successfully processed')
 
         finally:
             if os.path.exists(temp_file_path):
@@ -94,19 +95,15 @@ class DataCleaningTests(TestCase):
 
     # Check data is in the expected schema
     def test_data_schema(self):
-        upload, temp_file_path = self.create_test_upload()
+        temp_file_path = self.create_csv_file()
 
         try:
             # Run pipeline
-            outcome = run_cleaning_pipeline(upload.id)  # Pass specific upload ID
-            self.assertTrue(outcome['success'], 'Error: pipeline execution failed')
+            cleaner = DataCleaningPipeline()
+            df_cleaned, report = cleaner.clean_file(temp_file_path)
+            self.assertTrue(len(df_cleaned) > 0, 'Error: pipeline execution failed')
 
-            # Get records from database
-            records = DatasetRecord.objects.filter(data_upload=upload)
-            self.assertGreater(len(records), 0, 'Error: no record found in database')
-
-            expected_fields = [
-                'id',
+            expected_columns = [
                 'text',
                 'label',
                 'category_id',
@@ -114,111 +111,112 @@ class DataCleaningTests(TestCase):
                 'depression',
                 'suicidal',
                 'stress',
-                'dataset_type',
-                'data_upload',
-                'imported_at',
             ]
 
-            for record in records:
-                for field in expected_fields:
-                    self.assertTrue(hasattr(record, field), f'Missing field(s): {field}')
+            for column in expected_columns:
+                self.assertTrue(
+                    column in df_cleaned.columns, f'Missing field(s): {column}'
+                )
 
-            print("Correct Schema: All fields present")
+            print('Correct Schema: All expected columns are present')
 
         finally:
             if os.path.exists(temp_file_path):
                 os.unlink(temp_file_path)
 
-
-# Check data has no incorrect values (overly large/small statements, duplicates, invalid labels)
-# and correctly processes char encoding
+    # Check data has no incorrect values (overly large/small statements, duplicates, invalid labels)
+    # and correctly processes char encoding
 
     def test_invalid_text_values(self):
-        upload, temp_file_path = self.create_test_upload()
+        temp_file_path = self.create_csv_file()
 
         try:
-            outcome = run_cleaning_pipeline(upload.id)  # Run pipeline
-            self.assertTrue(outcome['success'], 'Error: pipeline execution failed')
+            cleaner = DataCleaningPipeline()
+            df_cleaned, report = cleaner.clean_file(temp_file_path)
+            self.assertTrue(len(df_cleaned) > 0, 'Error: pipeline execution failed')
 
-            records = DatasetRecord.objects.filter(data_upload=upload)
+            for text in df_cleaned['text']:
+                self.assertGreaterEqual(
+                    len(text), self.min_text_length
+                )  # Check min chars
+                self.assertGreaterEqual(
+                    len(text.split()), self.min_word_count
+                )  # Min words
+                self.assertLessEqual(len(text), self.max_text_length)  # Max chars
 
-            for record in records:
-                self.assertGreaterEqual(len(record.text), self.min_text_length)  # Check min chars
-                self.assertGreaterEqual(len(record.text.split()), self.min_word_count) # Min words
-                self.assertLessEqual(len(record.text), self.max_text_length) # Max chars
-
-
-            print("Text validation passed: Correct word length for every record")
+            print('Text validation passed: Correct word length for every record')
 
         finally:
             if os.path.exists(temp_file_path):
                 os.unlink(temp_file_path)
 
-
     def test_duplicate_values(self):
-        upload, temp_file_path = self.create_test_upload()
+        temp_file_path = self.create_csv_file()
 
         try:
-            outcome = run_cleaning_pipeline(upload.id)  # Run pipeline
-            self.assertTrue(outcome['success'], 'Error: pipeline execution failed')
+            cleaner = DataCleaningPipeline()
+            df_cleaned, report = cleaner.clean_file(temp_file_path)
+            self.assertTrue(len(df_cleaned) > 0, 'Error: pipeline execution failed')
 
-            records = DatasetRecord.objects.filter(data_upload=upload)
-            texts = [record.text for record in records]
+            texts = df_cleaned['text'].tolist()
 
             duplicate_texts = [text for text in set(texts) if texts.count(text) > 1]
 
-            self.assertEqual(len(duplicate_texts), 0, f'Error: Found duplicates: {duplicate_texts}')
-            print("Text validation passed: No duplicates")
+            self.assertEqual(
+                len(duplicate_texts), 0, f'Error: Found duplicates: {duplicate_texts}'
+            )
+            print('Text validation passed: No duplicates')
 
         finally:
             if os.path.exists(temp_file_path):
                 os.unlink(temp_file_path)
-
 
     def test_invalid_labels(self):
-        upload, temp_file_path = self.create_test_upload()
+        temp_file_path = self.create_csv_file()
 
         try:
-            outcome = run_cleaning_pipeline(upload.id)  # Run pipeline
-            self.assertTrue(outcome['success'], 'Error: pipeline execution failed')
+            cleaner = DataCleaningPipeline()
+            df_cleaned, report = cleaner.clean_file(temp_file_path)
+            self.assertTrue(len(df_cleaned) > 0, 'Error: pipeline execution failed')
 
-            records = DatasetRecord.objects.filter(data_upload=upload)
             invalid_labels = []
 
-            for record in records:
-                if record.label not in self.valid_labels:
-                    invalid_labels.append(f"'{record.label}' found in: '{record.text[:50]}...'")
+            for text, label in zip(df_cleaned['text'], df_cleaned['label']):
+                if label not in self.valid_labels:
+                    invalid_labels.append(
+                        f"'{label}' found in: '{text[:50]}...'"  # Get corresponding text of the invalid label
+                    )
 
-            self.assertEqual(len(invalid_labels), 0, 'Invalid labels found:\n' + '\n'.join(invalid_labels))
+            self.assertEqual(
+                len(invalid_labels),
+                0,
+                'Invalid labels found:\n' + '\n'.join(invalid_labels),
+            )
 
-
-            labels = [record.label for record in records] # Check for any Anxiety label
+            labels = df_cleaned['label'].tolist()  # Check for any Anxiety label
             self.assertNotIn('Anxiety', labels)
 
-            print("Labelling correct: All labels are valid")
+            print('Labelling correct: All labels are valid')
 
         finally:
             if os.path.exists(temp_file_path):
                 os.unlink(temp_file_path)
-
 
     def test_char_encoding(self):
-        upload, temp_file_path = self.create_test_upload()
+        temp_file_path = self.create_csv_file()
 
         try:
-            outcome = run_cleaning_pipeline(upload.id)  # Run pipeline
-            self.assertTrue(outcome['success'], 'Error: pipeline execution failed')
+            cleaner = DataCleaningPipeline()
+            df_cleaned, report = cleaner.clean_file(temp_file_path)
+            self.assertTrue(len(df_cleaned) > 0, 'Error: pipeline execution failed')
 
-            records = DatasetRecord.objects.filter(data_upload=upload)
+            for text in df_cleaned['text']:
+                self.assertNotIn('â€œ', text)  # Verify encoding fixes were applied
+                self.assertNotIn('Ã¼', text)
+                self.assertNotIn('\u2026', text)
 
-            for record in records:
-                self.assertNotIn('â€œ', record.text)    # Verify encoding fixes were applied
-                self.assertNotIn('Ã¼', record.text)
-                self.assertNotIn('\u2026', record.text)
-
-            print(f"Correct encoding: {len(records)} records cleaned")
+            print(f'Correct encoding: {len(df_cleaned)} records cleaned')
 
         finally:
             if os.path.exists(temp_file_path):
                 os.unlink(temp_file_path)
-
