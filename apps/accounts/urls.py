59e6fffd3167dc01bbc88c977176ae4088d229d7
Lian Shi <lians@student.chalmers.se>
2025-12-01 20:50:21 +0100

    #15 Add export-data api to allow user download their data
    Former-commit-id: d3ee2f72d26c353883ed3e6a24c4dba644fe08b5

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('api/change-password/', views.change_password_api, name='change_password_api'),
     path('api/delete-all-data/', views.delete_all_data_api, name='delete_all_data_api'),
     path('api/delete-account/', views.delete_account_api, name='delete_account_api'),
+    path('api/export-data/', views.export_data_api, name='export_data_api'),
     # Consent and privacy URLs
     path('consent/', views.consent_view, name='consent'),
     path('privacy/', views.privacy_policy_view, name='privacy_policy'),
