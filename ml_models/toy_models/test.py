5357c9d0f9c4241cca61bb59b07ac3e81908edee
byland <byland@student.chalmers.se>
2025-12-02 13:41:28 +0100

    #12 Delete ml_models directory
    Former-commit-id: 5ad6d55d3613ad8415c696f443887a76d08dd38b

--- ml_models/toy_models/test.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-model = joblib.load('ml_models/toy_models/LRmodel.pkl')
-
-prediction = model.predict(
-    ['I am stressed af, and I want to kill everything', 'Dragazo the conqueror']
-)
-print(prediction)
